b0a0e1ce88cfb3c1d44b4dece99464ddb54587f7
Allen Anderson <allenanderson@allens-mbp-2.lan>
2023-01-27 21:20:14 -0700

    working copy -no tests implemented

--- dessertshop.py
+++ dessertshop.py
@@ -1,29 +1,30 @@
 class DessertItem:
-    def __init__(self, name=''):
+    def __init__(self, name=str):
         self.name = name
 
 class Candy(DessertItem):
-    def __init__(self, name, candy_weight= float(0), price_per_pound=float(0)):
+    def __init__(self, name, candy_weight= float, price_per_pound=float):
         super().__init__(name)
         self.candy_weight = candy_weight
         self.price_per_pound = price_per_pound
 
 class Cookie(DessertItem):
-    def __init__(self, name, cookie_quantity, price_per_dozen):
+    def __init__(self, name, cookie_quantity=int, price_per_dozen=float):
         super().__init__(name)
         self.cookie_quantity = cookie_quantity
         self.price_per_dozen = price_per_dozen
 
 class IceCream(DessertItem):
-    def __init__(self, name, scoop_count, price_per_scoop):
+    def __init__(self, name, scoop_count=int, price_per_scoop=float):
         super().__init__(name)
         self.scoop_count = scoop_count
         self.price_per_scoop = price_per_scoop
 
-class Sundae(DessertItem, Icecream):
-    def __init__(self, name, scoop_count, price_per_scoop, topping_name, topping_price):
-        DessertItem.__init__(name)
-        IceCream.__init__(scoop_count, price_per_scoop)
+class Sundae(IceCream):
+    def __init__(self,name, scoop_count=int, price_per_scoop=float, topping_name=str, topping_price=float):
+        super().__init__(name, scoop_count, price_per_scoop)
         self.topping_name = topping_name
-        self.topping_price
-        
+        self.topping_price = topping_price
+
+
+ 
