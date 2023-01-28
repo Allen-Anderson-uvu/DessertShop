55512df969b9687ae11757d76577394d7f606cb1
Allen Anderson <allenanderson@allens-mbp-2.lan>
2023-01-27 18:44:06 -0700

    Initial Commit

---
+++ dessertshop.py
@@ -0,0 +1,29 @@
+class DessertItem:
+    def __init__(self, name=''):
+        self.name = name
+
+class Candy(DessertItem):
+    def __init__(self, name, candy_weight= float(0), price_per_pound=float(0)):
+        super().__init__(name)
+        self.candy_weight = candy_weight
+        self.price_per_pound = price_per_pound
+
+class Cookie(DessertItem):
+    def __init__(self, name, cookie_quantity, price_per_dozen):
+        super().__init__(name)
+        self.cookie_quantity = cookie_quantity
+        self.price_per_dozen = price_per_dozen
+
+class IceCream(DessertItem):
+    def __init__(self, name, scoop_count, price_per_scoop):
+        super().__init__(name)
+        self.scoop_count = scoop_count
+        self.price_per_scoop = price_per_scoop
+
+class Sundae(DessertItem, Icecream):
+    def __init__(self, name, scoop_count, price_per_scoop, topping_name, topping_price):
+        DessertItem.__init__(name)
+        IceCream.__init__(scoop_count, price_per_scoop)
+        self.topping_name = topping_name
+        self.topping_price
+        
