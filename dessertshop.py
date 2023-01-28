cf9de1b0739dfc277e130eaa2d6e50dd73ad5331
Allen Anderson <allenanderson@allens-mbp-2.lan>
2023-01-28 13:15:37 -0700

    Project 2 complete

--- dessertshop.py
+++ dessertshop.py
@@ -2,11 +2,17 @@ class DessertItem:
     def __init__(self, name=str):
         self.name = name
 
+    def __str__(self):
+        return self.name
+
 class Candy(DessertItem):
     def __init__(self, name, candy_weight= float, price_per_pound=float):
         super().__init__(name)
         self.candy_weight = candy_weight
         self.price_per_pound = price_per_pound
+
+        def __str__(self):
+            return self.name
 
 class Cookie(DessertItem):
     def __init__(self, name, cookie_quantity=int, price_per_dozen=float):
@@ -14,17 +20,26 @@ class Cookie(DessertItem):
         self.cookie_quantity = cookie_quantity
         self.price_per_dozen = price_per_dozen
 
+    def __str__(self):
+        return self.name
+
 class IceCream(DessertItem):
     def __init__(self, name, scoop_count=int, price_per_scoop=float):
         super().__init__(name)
         self.scoop_count = scoop_count
         self.price_per_scoop = price_per_scoop
+    
+    def __str__(self):
+        return self.name
 
 class Sundae(IceCream):
     def __init__(self,name, scoop_count=int, price_per_scoop=float, topping_name=str, topping_price=float):
         super().__init__(name, scoop_count, price_per_scoop)
         self.topping_name = topping_name
         self.topping_price = topping_price
+
+    def __str__(self):
+        return self.name
 
 class Order:
     def __init__(self):
