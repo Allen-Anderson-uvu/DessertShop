5b2d961d57d6b0c23981799f375a0d705f9bcbba
Allen Anderson <allenanderson@allens-mbp-2.lan>
2023-02-03 18:40:24 -0700

    update before I break it

--- dessertshop.py
+++ dessertshop.py
@@ -1,9 +1,19 @@
-class DessertItem:
+from abc import ABC, abstractmethod
+
+class DessertItem(ABC):
     def __init__(self, name=str):
         self.name = name
+        self.tax_percent = 7.25
 
     def __str__(self):
         return self.name
+
+    @abstractmethod
+    def calculate_cost(self):
+        pass
+
+    def calculate_tax(self):
+        return self.calculate_cost() * (self.tax_percent / 100)
 
 class Candy(DessertItem):
     def __init__(self, name, candy_weight= float, price_per_pound=float):
@@ -11,8 +21,9 @@ class Candy(DessertItem):
         self.candy_weight = candy_weight
         self.price_per_pound = price_per_pound
 
-        def __str__(self):
-            return self.name
+    def calculate_cost(self):
+        return self.candy_weight * self.price_per_pound
+
 
 class Cookie(DessertItem):
     def __init__(self, name, cookie_quantity=int, price_per_dozen=float):
@@ -20,17 +31,18 @@ class Cookie(DessertItem):
         self.cookie_quantity = cookie_quantity
         self.price_per_dozen = price_per_dozen
 
-    def __str__(self):
-        return self.name
+    def calculate_cost(self):
+        return self.cookie_quantity / 12 * self.price_per_dozen
 
 class IceCream(DessertItem):
     def __init__(self, name, scoop_count=int, price_per_scoop=float):
         super().__init__(name)
         self.scoop_count = scoop_count
         self.price_per_scoop = price_per_scoop
+
+    def calculate_cost(self):
+        return self.scoop_count * self.price_per_scoop
     
-    def __str__(self):
-        return self.name
 
 class Sundae(IceCream):
     def __init__(self,name, scoop_count=int, price_per_scoop=float, topping_name=str, topping_price=float):
@@ -38,31 +50,47 @@ class Sundae(IceCream):
         self.topping_name = topping_name
         self.topping_price = topping_price
 
-    def __str__(self):
-        return self.name
+    def calculate_cost(self):
+        return super().calculate_cost() + self.topping_price
 
 class Order:
     def __init__(self):
         self.order = []
 
-    def orderitem(self, item):
+    def add_item(self, item):
         self.order.append(item)
  
     def __len__(self):
         return len(self.order)
 
-def main():
-    neworder = Order()
-    neworder.orderitem(Candy("Candy Corn", 1.5, .25))
-    neworder.orderitem(Candy("Gummy Bears", .25, .35))
-    neworder.orderitem(Cookie("Chocolate Chip", 6, 3.99))
-    neworder.orderitem(IceCream("Pistachio", 2, .79))
-    neworder.orderitem(Sundae("Vanilla", 3, .69, "Hot Fudge", 1.29))
-    neworder.orderitem(Cookie("Oatmeal Raisin", 2, 3.45))
+    def order_cost(self):
+        total_cost = 0
+        for items in self.order:
+            total_cost += items.calculate_cost()
+        return round(total_cost, 2)
 
-    for item in neworder.order:
-        print(item)
-    print('Total number of items in order:', len(neworder.order))
+    def order_tax(self):
+        total_tax = 0
+        for item in self.order:
+            total_tax += item.calculate_tax()
+
+        return round(total_tax, 2)
+
+def main():
+    gummybears = Candy("Gummy Bears", 2.3, 1.25)
+    chocolate_cookies = Cookie("Chocolate Cookies", 12, 2.5)
+    vanilla_icecream = IceCream("Vanilla Ice Cream", 3, 1.75)
+    strawberry_sundae = Sundae("Strawberry Sundae", 2, 2, "Strawberry Topping", 0.5)
+
+    order = Order()
+    order.add_item(gummybears)
+    order.add_item(chocolate_cookies)
+    order.add_item(vanilla_icecream)
+    order.add_item(strawberry_sundae)
+
+    print(order.order_cost())
+    print(order.order_tax)
+
 
 if __name__ == "__main__":
     main()
